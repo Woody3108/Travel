de78c44cece8b27aed9c92136d6e8bfbdc16bf27
Woods3108 <2920892312@qq.com>
2019-05-04 19:04:28 +0800

    City CSS Finish ( CityAlphabet + CityList )

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,24 @@
     <div>
         <CityHeader>city</CityHeader>
         <CitySearch></CitySearch>
+        <CityList></CityList>
+        <CityAlphabet></CityAlphabet>
     </div>
 </template>
 
 <script>
     import CityHeader from './components/CityHeader'
     import CitySearch from './components/CitySearch'
+    import CityList from './components/CityList'
+    import CityAlphabet from './components/CityAlphabet'
 
     export default {
         name: 'City',
         components: {
             CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            CityAlphabet
         }
     }
 </script>
