487f1525dffac1b7401dbaa3624b9ef90ecce6c7
Woods3108 <2920892312@qq.com>
2019-05-05 22:40:37 +0800

    add CityList

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,12 +2,13 @@
     <div>
         <CityHeader>city</CityHeader>
         <CitySearch></CitySearch>
-        <CityList></CityList>
-        <CityAlphabet></CityAlphabet>
+        <CityList :cities="cities" :hot="hotCities"></CityList>
+        <CityAlphabet :cities="cities"></CityAlphabet>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     import CityHeader from './components/CityHeader'
     import CitySearch from './components/CitySearch'
     import CityList from './components/CityList'
@@ -20,6 +21,34 @@
             CitySearch,
             CityList,
             CityAlphabet
+        },
+        data () {
+            return {
+                cities: {},
+                hotCities: []
+            }
+        },
+        methods: {
+            getCityInfo () {
+                axios.get('/api/city.json')
+                    .then(this.handleGetCityInfoSucc)
+            },
+            handleGetCityInfoSucc (res) {
+                let r = res.data
+                // ret 表示数据请求是否成功；布尔值；
+                if (r.ret && r.data) {
+                    // console.log('r.ret', r.ret)
+                    let data = r.data
+                    // console.log('data', data)
+                    this.cities = data.cities
+                    this.hotCities = data.hotCities
+                    // console.log('cities', this.cities)
+                    // console.log('hotCities', this.hotCities)
+                }
+            }
+        },
+        mounted () {
+            this.getCityInfo()
         }
     }
 </script>
